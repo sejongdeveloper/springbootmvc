dd0eb7146568863dfb1d52615f95fd2ce741222e
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-07 12:14:08 +0900

    스프링 웹 MVC 7부: Thymeleaf

---
+++ src/test/java/me/whiteship/springbootmvc/SpringbootmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.springbootmvc;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringbootmvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
